d7d9ca01b9fce123f5bb0e31a647bf086dc2a204
Seu <helielsouza01234@gmail.com>
2024-04-13 11:32:33 -0300

    Layout changes and js modified to content div

--- assets/js/carrousel.js
+++ assets/js/carrousel.js
@@ -2,6 +2,7 @@ let items = document.querySelectorAll('.slider .list .item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 let thumbnails = document.querySelectorAll('.thumbnail .item');
+let content = document.querySelectorAll('.content .item');
 
 // config param
 let countItem = items.length;
@@ -25,23 +26,35 @@ prev.onclick = function(){
 // auto run slider
 let refreshInterval = setInterval(() => {
     next.click();
-}, 5000)
+}, 3000)
 function showSlider(){
     // remove item active old
     let itemActiveOld = document.querySelector('.slider .list .item.active');
     let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    let contentActiveOld = document.querySelector('.slider .content .item.active');
+
     itemActiveOld.classList.remove('active');
     thumbnailActiveOld.classList.remove('active');
+    contentActiveOld.classList.remove('active');
 
     // active new item
     items[itemActive].classList.add('active');
     thumbnails[itemActive].classList.add('active');
+    content[itemActive].classList.add('active');
+
+    // Esconder todos os textos na .content
+    document.querySelectorAll('.content .item').forEach(item => {
+        item.style.display = 'none';
+    });
+    // Mostrar apenas o texto correspondente ao item ativo
+    document.querySelector(`.content .content-item-${itemActive}`).style.display = 'block';
+
 
     // clear auto time run slider
     clearInterval(refreshInterval);
     refreshInterval = setInterval(() => {
         next.click();
-    }, 5000)
+    }, 3000)
 }
 
 // click thumbnail
