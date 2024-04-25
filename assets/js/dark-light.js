cf94540f6e0de04764b147b32d32bac59313a011
Heliel Souza <helielsouza01234@gmail.com>
2024-04-25 14:06:47 -0300

    100% of dark-light theme implemented

--- assets/js/dark-light.js
+++ assets/js/dark-light.js
@@ -1,7 +1,37 @@
 const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
+    const darkLogo = document.querySelector('.logo .logo-dark');
+    const lightLogo = document.querySelector('.logo .logo-light');
+    const darkDev = document.querySelector('.intro-img .developer-dark');
+    const lightDev = document.querySelector('.intro-img .developer-light');
+    const darkContact = document.querySelector('.contacts-img .contact-dark');
+    const lightContact = document.querySelector('.contacts-img .contact-light');
+
+    if (chk.checked) {
+        darkLogo.style.display = 'none';
+        lightLogo.style.display = 'inline-block';
+        darkDev.style.display = 'none';
+        lightDev.style.display = 'inline-block';
+        darkContact.style.display = 'none';
+        lightContact.style.display = 'inline-block';
+    } else {
+        darkLogo.style.display = 'inline-block';
+        lightLogo.style.display = 'none';
+        darkDev.style.display = 'inline-block';
+        lightDev.style.display = 'none';
+        darkContact.style.display = 'inline-block';
+        lightContact.style.display = 'none';
+    }
     document.querySelector('.dark-bg-svg').classList.toggle('light-bg-svg');
+    document.querySelector('.dark-bg-menu').classList.toggle('light-bg-menu');
+    document.querySelectorAll('.menu ul li a').forEach(h2 => {
+        h2.classList.toggle('dark-color');
+    })
+    document.querySelectorAll('.menu ul li a').forEach(h2 => {
+        h2.classList.toggle('crimson-color-hover');
+    })
+
     document.querySelectorAll('.neonText').forEach(h2 => {
         h2.classList.toggle('dark-color');
     })
@@ -53,5 +83,29 @@ chk.addEventListener('change', () => {
     document.querySelectorAll('.card').forEach(card_sdhw => {
         card_sdhw.classList.toggle('card-dark-shadow');
     });
-    
+    document.querySelectorAll('form legend').forEach(form_lgnd => {
+        form_lgnd.classList.toggle('dark-color');
+    });
+    document.querySelectorAll('form label').forEach(form_lgnd => {
+        form_lgnd.classList.toggle('dark-color');
+    });
+    document.querySelectorAll('.form-group input').forEach(form_lgnd => {
+        form_lgnd.classList.toggle('crimson-color-border');
+    });
+    document.querySelectorAll('.form-group textarea').forEach(form_lgnd => {
+        form_lgnd.classList.toggle('crimson-color-border');
+    });
+    document.querySelectorAll('.form-group input').forEach(form_lgnd => {
+        form_lgnd.classList.toggle('crimson-box-shadow');
+    });
+    document.querySelectorAll('.form-group textarea').forEach(form_lgnd => {
+        form_lgnd.classList.toggle('crimson-box-shadow');
+    });
+    document.querySelectorAll('.white-text').forEach(form_lgnd => {
+        form_lgnd.classList.toggle('dark-text-footer');
+    });
+    document.querySelectorAll('footer a').forEach(form_lgnd => {
+        form_lgnd.classList.toggle('dark-text-footer');
+    });
+    document.querySelector('.form-group button').classList.toggle('form-button-light');
 });
